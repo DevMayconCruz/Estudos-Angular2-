877993aa5bac56ff1c976f8ff74e8a01bd96fd21
Maycon Cruz <dev.mayconcruz@gmail.com>
2021-11-10 15:54:11 -0300

    Olá, mundo!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meu-primeiro-app';
+ 
+  public title = "Olá, Mundo!";
+  public nome = " Maycon";
+  public sobreNome = "Cruz";
 }
